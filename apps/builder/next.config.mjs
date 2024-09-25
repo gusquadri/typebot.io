e72bc9c91e1b8b76348b81b35db9a35706dd8d3b
gusquadri <87215048+gusquadri@users.noreply.github.com>
2024-09-25 10:56:34 -0300

    xframe 2

--- apps/builder/next.config.mjs
+++ apps/builder/next.config.mjs
@@ -66,7 +66,7 @@ const nextConfig = {
         headers: [
           {
             key: 'X-Frame-Options',
-            value: 'SAMEORIGIN',
+            value: 'ALLOWALL',
           },
         ],
       },
